352332298d55caeb9599c394c32938eb05675665
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-05-25 18:15:29 +0200

    Add project files.

---
+++ Data/Factories/DataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+
+namespace Data.Factories;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+	public DataContext CreateDbContext(string[] args)
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+		optionsBuilder.UseSqlServer("Server=tcp:sqlserver-silicon-js.database.windows.net,1433;Initial Catalog=accounts;Persist Security Info=False;User ID=SqlAdmin;Password=oG%77_sE123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+		return new DataContext(optionsBuilder.Options);
+	}
+}
